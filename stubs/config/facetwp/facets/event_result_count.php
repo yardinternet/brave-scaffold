2c5f3c294956c4b18234b4b4521b972a47290b39
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2025-11-10 16:59:30 +0100

    chore: i8n

--- stubs/config/facetwp/facets/event_result_count.php
+++ stubs/config/facetwp/facets/event_result_count.php
@@ -7,6 +7,7 @@ return [
 	'label' => 'Resultaten evenementen',
 	'type' => 'pager',
 	'pager_type' => 'counts',
+	/* translators: [total] is replaced with result count */
 	'count_text_plural' => __('Er zijn <span>[total]</span> evenementen gevonden.', 'sage'),
 	'count_text_singular' => __('Er is <span>1</span> evenement gevonden.', 'sage'),
 	'count_text_none' => __('Er zijn geen evenementen gevonden.', 'sage'),
