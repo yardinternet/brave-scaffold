4675799ebe7f8d6344e18d7bab5d288c54db8d94
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2025-11-11 15:07:13 +0100

    refactor: adjust eventData address block

--- stubs/resources/views/vendor/wp-events/blocks/event-info.blade.php
+++ stubs/resources/views/vendor/wp-events/blocks/event-info.blade.php
@@ -25,20 +25,16 @@
 		<li class="flex items-baseline gap-x-4 gap-y-2">
 			<i class="fa-light text-primary fa-fw fa-map-marker-alt min-w-5"></i>
 			<span class="flex flex-col">
-				@if ($eventData->isOnline())
-					{!! $eventData->isOnlineLabel() !!}
-				@elseif($eventData->venue())
+				@if($eventData->venue())
 					<span>{!! $eventData->venue()?->title() !!}</span>
-					<span>{!! $eventData->venue()?->location?->formattedAddress()!!}</span>
-					@if ($eventData->venue()?->location?->googleMapsLink())
-						<span>
+				@endif
+				{!! $eventData->formattedAddress !!}
+				@if ($eventData->venue()?->location?->googleMapsLink())
+					<span>
 							<a href="{{ $eventData->venue()?->location?->googleMapsLink() }}" target="_blank">
 								{{ __('Bekijk op Google Maps', 'sage') }}
 							</a>
 						</span>
-					@endif
-				@else
-					{!! $eventData->locationUnknown() !!}
 				@endif
 			</span>
 		</li>
