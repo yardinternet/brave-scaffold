dc0bd173ab0cdb2c066c5a7115a6842e593c5d72
Lara van Diemen <lara@yard.nl>
2026-04-13 12:08:39 +0200

    fix: change facetwp loops from grid to flex

--- stubs/resources/views/blocks/FacetWP/loops/knowledgebase-loop.blade.php
+++ stubs/resources/views/blocks/FacetWP/loops/knowledgebase-loop.blade.php
@@ -1,9 +1,9 @@
 @use(App\View\Components\Card\Enums\Direction)
 
-<div class="@xl:gap-6 grid gap-4">
-	@forelse ($postDataCollection as $postData)
-		<x-card.knowledgebase :postData="$postData" :direction="Direction::FLUID" />
-	@empty
-		<x-facetwp.no-results />
-	@endforelse
+<div class="@xl:gap-6 flex flex-col gap-4">
+    @forelse ($postDataCollection as $postData)
+        <x-card.knowledgebase :postData="$postData" :direction="Direction::FLUID" />
+    @empty
+        <x-facetwp.no-results />
+    @endforelse
 </div>
--- stubs/resources/views/blocks/FacetWP/loops/project-loop.blade.php
+++ stubs/resources/views/blocks/FacetWP/loops/project-loop.blade.php
@@ -1,9 +1,9 @@
 @use(App\View\Components\Card\Enums\Direction)
 
-<div class="@xl:gap-6 grid gap-4">
-	@forelse ($postDataCollection as $postData)
-		<x-card.project :postData="$postData" :direction="Direction::FLUID" />
-	@empty
-		@include('blocks.FacetWP.partials.no-results')
-	@endforelse
+<div class="@xl:gap-6 flex flex-col gap-4">
+    @forelse ($postDataCollection as $postData)
+        <x-card.project :postData="$postData" :direction="Direction::FLUID" />
+    @empty
+        @include('blocks.FacetWP.partials.no-results')
+    @endforelse
 </div>
