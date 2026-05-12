7ff36244ab5a68f6147cd5260071f3f718a96ab5
Wybe van den Bosch <wybe.vandenbosch+git@gmail.com>
2026-05-12 09:58:59 +0200

    fix: only template not using auto-grid yet

--- stubs/resources/views/blocks/FacetWP/loops/person-loop.blade.php
+++ stubs/resources/views/blocks/FacetWP/loops/person-loop.blade.php
@@ -1,4 +1,4 @@
-<div class="@xl:gap-6 @lg:grid-cols-2 @3xl:grid-cols-3 grid gap-4">
+<div class="auto-grid">
 	@forelse ($postDataCollection as $postData)
 		<x-card.person :postData="$postData" />
 	@empty
